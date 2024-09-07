cdcb9cd86c95a6f19780587582b482fec1ccada0
현철 <g9191hc@gmail.com>
2024-09-07 22:24:35 +0900

    feat: shadcn-svelte 적용(Buttons) 및 lucide 적용

---
+++ src/lib/components/ui/buttons.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Button } from "./button";
+  import Mail from "lucide-svelte/icons/mail";
+  import Chevronright from "lucide-svelte/icons/chevron-right";
+  import { LoaderCircle } from "lucide-svelte";
+</script>
+
+<h1>Buttons</h1>
+<div class="flex flex-col items-center">
+  <Button>default</Button>
+  <Button variant="secondary">secondary</Button>
+  <Button variant="destructive">destructive</Button>
+  <Button variant="outline">outline</Button>
+  <Button variant="ghost">ghost</Button>
+  <Button variant="link">link</Button>
+  <Button disabled>
+    <LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+    loading animation(disabled)
+  </Button>
+  <Button>
+    <Mail class="mr-2 h-4 w-4" />
+    Button with Icon
+  </Button>
+  <div>
+    <Button variant="outline" size="icon">
+      <Chevronright class="h-4 w-4" />
+    </Button>
+    size="icon"
+  </div>
+</div>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,3 @@
+// 아래 두개는 tauri 필수셋팅
 export const prerender = true
 export const ssr = false
